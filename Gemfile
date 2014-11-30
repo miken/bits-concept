4f22335558262f8e84957fae85e23eb0f6111f28
Mike Nguyen <mike@toch.im>
2014-11-30 11:11:48 -0500

    Initial set up

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'http://rubygems.org'
+ruby '2.1.3'
+
+gem 'jekyll', '~> 1.3.0'
+gem 'jekyll-slim'
+gem 'jekyll-asset-pipeline'
+gem 'compass'
+gem 'zurb-foundation'
+gem 'yui-compressor'
+gem 'coffee-script'
+
+gem 'rake'
+gem 'puma'
+gem 'rack-contrib'
+gem 'rack-rewrite'
+
+group :development  do
+  gem 'guard-jekyll-plus', git: 'https://github.com/imathis/guard-jekyll-plus', branch: 'master'
+  gem 'rb-fsevent'
+  gem 'guard-livereload'
+end
---
+++ src/_plugins/pipeline.rb
@@ -0,0 +1,52 @@
+require 'jekyll_asset_pipeline'
+require 'compass'
+require 'zurb-foundation'
+
+Compass.sass_engine_options[:load_paths].each do |path|
+  Sass.load_paths << path
+end
+
+module JekyllAssetPipeline
+
+  # process SCSS files
+  class SassConverter < JekyllAssetPipeline::Converter
+
+    Compass.configuration.sass_dir = 'src/_assets/css'
+
+    Compass.sass_engine_options[:load_paths].each do |path|
+      Sass.load_paths << path
+    end
+
+    def self.filetype
+      '.scss'
+    end
+
+    def convert
+      Sass::Engine.new(@content, syntax: :scss).render
+    end
+  end
+
+  class CssCompressor < JekyllAssetPipeline::Compressor
+    require 'yui/compressor'
+
+    def self.filetype
+      '.css'
+    end
+
+    def compress
+      YUI::CssCompressor.new.compress(@content)
+    end
+  end
+
+  class CoffeeScriptConverter < JekyllAssetPipeline::Converter
+    require 'coffee-script'
+
+    def self.filetype
+      '.coffee'
+    end
+
+    def convert
+      return CoffeeScript.compile(@content)
+    end
+  end
+end
